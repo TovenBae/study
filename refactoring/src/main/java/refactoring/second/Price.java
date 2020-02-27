7d55ecfdf1dc567e03ffa94a500765587a39d6f4
배성한 <furtur3@gmail.com>
2020-02-27 19:32:06 +0900

    move getCharge to Price class

--- refactoring/src/main/java/refactoring/second/Price.java
+++ refactoring/src/main/java/refactoring/second/Price.java
@@ -2,6 +2,29 @@ package refactoring.second;
 
 public abstract class Price {
 	abstract int getPriceCode();
+	
+	public double getCharge(int daysRented) {
+		double result = 0;
+		// 비디오 종류별 대여료 계산
+		switch (getPriceCode()) {
+			case Movie.REGULAR:
+				result += 2;
+				if (daysRented > 2) {
+					result += (daysRented -2) * 1.5;
+				}
+				break;
+			case Movie.NEW_RELEASE:
+				result += daysRented * 3;
+				break;
+			case Movie.CHILDRENS:
+				result += 1.5;
+				if (daysRented > 3) {
+					result += (daysRented -3) * 1.5;
+				}
+				break;
+		}
+		return result;
+	}
 }
 
 
