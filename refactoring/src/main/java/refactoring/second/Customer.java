77f52efd1e37ecc07f6f765815c4036ca5c5139a
배성한 <furtur3@gmail.com>
2020-02-27 18:54:14 +0900

    add second package

---
+++ refactoring/src/main/java/refactoring/second/Customer.java
@@ -0,0 +1,58 @@
+package refactoring.second;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Customer {
+	private String _name;
+	private Vector _rentals = new Vector();
+	
+	public Customer(String name) {
+		_name = name;
+	}
+	public void addRental(Rental arg) {
+		_rentals.addElement(arg);
+	}
+	public String getName() {
+		return _name;
+	}
+	public String statement() {
+		Enumeration rentals = _rentals.elements();
+		String result = getName() + " 고객님의 대여 기록\n";
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			
+			// 이번에 대여하는 비디오 정보와 대여료를 출력
+			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+			
+		}
+		
+		// footer 행 추가
+		result += "누적 대여료 : " + String.valueOf(getTotalCharge()) + "\n";
+		result += "적립 포인트 : " + String.valueOf(getTotalFrequentRenterPoints());
+		return result;
+	}
+	
+	private double getTotalCharge() {
+		double result = 0;
+		Enumeration rentals = _rentals.elements();
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			// 현재까지 누적된 총 대여료
+			result += each.getCharge();
+		}
+		return result;
+	}
+	
+	private int getTotalFrequentRenterPoints() {
+		int result = 0;
+		Enumeration rentals = _rentals.elements();
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			// 현재까지 누적된 총 대여료
+			result += each.getFrequentRenterPoints();
+		}
+		return result;
+	}
+	
+}
---
+++ refactoring/src/main/java/refactoring/second/Movie.java
@@ -0,0 +1,25 @@
+package refactoring.second;
+
+public class Movie {
+	public static final int CHILDRENS = 2;
+	public static final int REGULAR = 0;
+	public static final int NEW_RELEASE = 1;
+	private String _title;
+	private int _priceCode;
+	
+	public Movie(String title, int priceCode) {
+		_title = title;
+		_priceCode = priceCode;
+	}
+	
+	public int getPriceCode() {
+		return _priceCode;
+	}
+	
+	public void getPriceCode(int priceCode) {
+		this._priceCode = priceCode;
+	}
+	public String getTitle() {
+		return _title;
+	}
+}
