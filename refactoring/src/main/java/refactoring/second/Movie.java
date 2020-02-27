1d4daf1cff9f4f164c003a71a2d68e4313932527
배성한 <furtur3@gmail.com>
2020-02-27 19:02:48 +0900

    move method getFrequentRenterPoints -> Movie class

--- refactoring/src/main/java/refactoring/second/Movie.java
+++ refactoring/src/main/java/refactoring/second/Movie.java
@@ -45,4 +45,13 @@ public class Movie {
 		}
 		return result;
 	}
+	
+	public int getFrequentRenterPoints(int daysRented) {
+		// 최신물이면서 2일이상 대여시 2포인트, 그외는 1포인트 적립.
+		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
+			return 2;
+		} else {
+			return 1;
+		}
+	}
 }
--- refactoring/src/main/java/refactoring/second/Rental.java
+++ refactoring/src/main/java/refactoring/second/Rental.java
@@ -21,11 +21,6 @@ public class Rental {
 	}
 	
 	public int getFrequentRenterPoints() {
-		// 최신물이면서 2일이상 대여시 2포인트, 그외는 1포인트 적립.
-		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
-			return 2;
-		} else {
-			return 1;
-		}
+		return _movie.getFrequentRenterPoints(getDaysRented());
 	}
 }
