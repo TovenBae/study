6871e902524b8dd3a37cee0a38350d56aece9f57
배성한 <furtur3@gmail.com>
2020-02-27 18:49:03 +0900

    remove variable frequentRentalPoints

--- refactoring/src/main/java/refactoring/first/Customer.java
+++ refactoring/src/main/java/refactoring/first/Customer.java
@@ -20,13 +20,10 @@ public class Customer {
 		return _name;
 	}
 	public String statement() {
-		int frequentRenterPoints = 0;
 		Enumeration rentals = _rentals.elements();
 		String result = getName() + " 고객님의 대여 기록\n";
 		while (rentals.hasMoreElements()) {
 			Rental each = (Rental) rentals.nextElement();
-			
-			frequentRenterPoints += each.getFrequentRenterPoints();
 			
 			// 이번에 대여하는 비디오 정보와 대여료를 출력
 			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
@@ -35,7 +32,7 @@ public class Customer {
 		
 		// footer 행 추가
 		result += "누적 대여료 : " + String.valueOf(getTotalCharge()) + "\n";
-		result += "적립 포인트 : " + String.valueOf(frequentRenterPoints);
+		result += "적립 포인트 : " + String.valueOf(getTotalFrequentRenterPoints());
 		return result;
 	}
 	
@@ -46,6 +43,17 @@ public class Customer {
 			Rental each = (Rental) rentals.nextElement();
 			// 현재까지 누적된 총 대여료
 			result += each.getCharge();
+		}
+		return result;
+	}
+	
+	private double getTotalFrequentRenterPoints() {
+		double result = 0;
+		Enumeration rentals = _rentals.elements();
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			// 현재까지 누적된 총 대여료
+			result += each.getFrequentRenterPoints();
 		}
 		return result;
 	}
