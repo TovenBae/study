b7f66468057c3ada98b323b1d4109cc3873b5f0d
배성한 <furtur3@gmail.com>
2020-02-27 19:39:35 +0900

    move getCharge to Child class of Price

--- refactoring/src/main/java/refactoring/second/ChildrenPrice.java
+++ refactoring/src/main/java/refactoring/second/ChildrenPrice.java
@@ -4,4 +4,11 @@ class ChildrenPrice extends Price {
 	int getPriceCode() {
 		return Movie.CHILDRENS;
 	}
+	public double getCharge(int daysRented) {
+		double result = 1.5;
+		if (daysRented > 3) {
+			result += (daysRented -3) * 1.5;
+		}
+		return result;
+	}
 }
--- refactoring/src/main/java/refactoring/second/RegularPrice.java
+++ refactoring/src/main/java/refactoring/second/RegularPrice.java
@@ -4,4 +4,11 @@ class RegularPrice extends Price {
 	int getPriceCode() {
 		return Movie.REGULAR;
 	}
+	public double getCharge(int daysRented) {
+		double result = 2;
+		if (daysRented > 2) {
+			result += (daysRented -2) * 1.5;
+		}
+		return result;
+	}
 }
