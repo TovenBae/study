e9a8aa49ca8d9ffc6dbe95329ba8483e803afd6c
TovenBae <furtur3@gmail.com>
2021-07-12 18:39:18 +0900

    add leetcode : easy

--- LeetCode/src/main/java/easy/RemoveDuplicatesfromSortedArray.java
+++ LeetCode/src/main/java/easy/RemoveDuplicatesfromSortedArray.java
@@ -2,9 +2,13 @@ package easy;
 
 // https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
-//Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
-//        Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
-//        Return k after placing the final result in the first k slots of nums.
+//Given an integer array nums sorted in non-decreasing order,
+// remove the duplicates in-place such that each unique element appears only once.
+// The relative order of the elements should be kept the same.
+// Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+// Return k after placing the final result in the first k slots of nums.
+
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,11 +18,24 @@ import static org.junit.Assert.assertEquals;
 //        nums is sorted in non-decreasing order.
 public class RemoveDuplicatesfromSortedArray {
     public int removeDuplicates(int[] nums) {
-        int rtn = 0;
+        if (nums == null || nums.length == 0) return 0;
 
+        int rtn = 1;
+        int oldPos = 0;
+        int oldVal = nums[oldPos];
+        for (int i=1; i<nums.length; i++) {
+            if (oldVal == nums[i]) {
+                continue;
+            } else {
+                nums[++oldPos] = nums[i];
+                oldVal = nums[i];
+                rtn++;
+            }
+        }
         return rtn;
     }
 
+    @Test
     public void test() {
         RemoveDuplicatesfromSortedArray test = new RemoveDuplicatesfromSortedArray();
         int [] nums = {1, 1, 2};
@@ -26,21 +43,39 @@ public class RemoveDuplicatesfromSortedArray {
         int [] expectedNums = {1,2};
         assertEquals(Output, test.removeDuplicates(nums));
 
-        assert Output == expectedNums.length;
-        for (int i = 0; i < Output; i++) {
-            assert nums[i] == expectedNums[i];
-        }
+        checkResult(Output, nums, expectedNums);
 
         nums = new int[] {0,0,1,1,1,2,2,3,3,4};
         Output = 5; //
         expectedNums = new int[] {0,1,2,3,4};// [0,1,2,3,4,_,_,_,_,_]
         assertEquals(Output, test.removeDuplicates(nums));
 
+        checkResult(Output, nums, expectedNums);
+
+        int [] nums2 = null;
+        Output = 0; //
+        int [] expectedNums2 = null;
+        assertEquals(Output, test.removeDuplicates(nums2));
+
+        checkResult(Output, nums2, expectedNums2);
+
+
+        int [] nums1 = {};
+        Output = 0; //
+        int [] expectedNums1 = {}; // [];
+        assertEquals(Output, test.removeDuplicates(nums1));
+
+        checkResult(Output, nums1, expectedNums1);
+
+    }
+
+    private void checkResult(int Output, int [] nums, int [] expectedNums) {
+        if (nums == null)
+            return;
+
         assert Output == expectedNums.length;
         for (int i = 0; i < Output; i++) {
             assert nums[i] == expectedNums[i];
         }
-
     }
-
 }
