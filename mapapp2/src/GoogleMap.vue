b132e92921256ae075da0ce71e7c2cba7c0aeba9
TovenBae <furtur3@gmail.com>
2021-08-27 18:11:38 +0900

    developing, add vue2-google-maps

--- mapapp2/src/GoogleMap.vue
+++ mapapp2/src/GoogleMap.vue
@@ -1,7 +1,24 @@
 <template>
     <div>
         <h1>Map is here</h1>
-        <div id="map" class="h-250"></div>
+        <GmapMap
+            :center="center"
+            :zoom="7"
+            map-type-id="terrain"
+            style="width: 500px; height: 300px"
+            >
+            <gmap-info-window :options="infoOptions" :position="infoWindowPos" :opened="infoWinOpen" @closeclick="infoWinOpen=false">
+                {{infoContent}}
+            </gmap-info-window>
+            <GmapMarker
+                :key="index"
+                v-for="(m, index) in markers"
+                :position="m.position"
+                :clickable="true"
+                :draggable="true"
+                @click="toggleInfoWindow(m,index)"
+            />
+        </GmapMap>
     </div>
 </template>
 
@@ -39,16 +56,6 @@ export default  {
             }
         }
     },
-    mounted() {
-        this.$markers = []
-        this.$map = new google.maps.Map(document.getElementById('map'), {
-            center: new google.maps.LatLng(this.latitude, this.longitude),
-            zoom: this.zoom
-        })
-        Vue.nextTick().then(() => {
-            this.clearMarkers()
-        })
-    },
     created() {
         EventBus.$on('clear-markers', () => {
             this.clearMarkers
@@ -60,7 +67,37 @@ export default  {
         })
     },
     data() {
-        return {}
+        return {
+            infoContent: '',
+            infoWindowPos: null,
+            infoWinOpen: false,
+            currentMidx: null,
+            //optional: offset infowindow so it visually sits nicely on top of our marker
+            infoOptions: {
+                pixelOffset: {
+                width: 0,
+                height: -35
+                }
+            },
+            center: {
+                lat: 37.505205,
+                lng: 127.049554
+            },
+            markers: [
+                {
+                    position: {
+                        lat: 37.505205,
+                        lng: 127.049554
+                    },
+                    infoText: '<img src="https://www.google.co.kr/imgres?imgurl=https%3A%2F%2Fcdn.imweb.me%2Fupload%2FS201905295cee7c0f94cee%2F12d4d58e92dd7.jpeg&imgrefurl=https%3A%2F%2Ftreepla.net%2Fmagazine%2F%3Fq%3DYToxOntzOjEyOiJrZXl3b3JkX3R5cGUiO3M6MzoiYWxsIjt9%26bmode%3Dview%26idx%3D4154746%26t%3Dboard&tbnid=aBhRlymOQw1SDM&vet=12ahUKEwiC1uzC7dDyAhVFYpQKHRioB4kQMygAegUIARDHAQ..i&docid=J104F_QUXK-28M&w=1061&h=1061&q=%EC%8B%9D%EB%AC%BC&hl=ko&ved=2ahUKEwiC1uzC7dDyAhVFYpQKHRioB4kQMygAegUIARDHAQ">'
+                }, {
+                    position: {
+                        lat: 37.505205,
+                        lng: 128.049554
+                    },
+                    infoText: 'Marker 2'
+                }]
+        }
     },
     methods: {
         makeMarker(latitude, longitude) {
@@ -75,11 +112,24 @@ export default  {
             for(let i=0; i<this.$markers.length; i++) {
                 this.$markers[i].setMap(null)
             }
+        },
+        toggleInfoWindow: function(marker, idx) {
+            this.infoWindowPos = marker.position;
+            this.infoContent = marker.infoText;
+
+            //check if its the same marker that was selected if yes toggle
+            if (this.currentMidx == idx) {
+                this.infoWinOpen = !this.infoWinOpen;
+            }
+            //if different marker set infowindow to open and reset current marker index
+            else {
+                this.infoWinOpen = true;
+                this.currentMidx = idx;
+
+            }
         }
+
     }
-
-
-
 }
 </script>
 
--- mapapp2/src/main.js
+++ mapapp2/src/main.js
@@ -3,6 +3,32 @@ import App from './App.vue'
 import GoogleMap from './GoogleMap'
 import ListView from './ListView'
 import Sidebar from './Sidebar'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyD4c0hkElbhOlKPODFrAj6ieKIGQP10IGc',
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+ 
+    //// If you want to set the version, you can do so:
+    // v: '3.26',
+  },
+ 
+  //// If you intend to programmatically custom event listener code
+  //// (e.g. `this.$refs.gmap.$on('zoom_changed', someFunc)`)
+  //// instead of going through Vue templates (e.g. `<GmapMap @zoom_changed="someFunc">`)
+  //// you might need to turn this on.
+  // autobindAllEvents: false,
+ 
+  //// If you want to manually install components, e.g.
+  //// import {GmapMarker} from 'vue2-google-maps/src/components/marker'
+  //// Vue.component('GmapMarker', GmapMarker)
+  //// then disable the following:
+  // installComponents: true,
+})
 
 window.EventBus = new Vue({
   data() {
