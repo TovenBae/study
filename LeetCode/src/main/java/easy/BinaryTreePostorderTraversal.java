3d1bd6c653e2d315c0331589fce3bb49368d18dd
TovenBae <furtur3@gmail.com>
2021-08-31 18:04:42 +0900

    add leetcode

--- LeetCode/src/main/java/easy/BinaryTreePostorderTraversal.java
+++ LeetCode/src/main/java/easy/BinaryTreePostorderTraversal.java
@@ -1,13 +1,14 @@
 package easy;
 
 /**
- https://leetcode.com/problems/binary-tree-preorder-traversal/
 
- Given the root of a binary tree, return the preorder traversal of its nodes' values.
+ https://leetcode.com/problems/binary-tree-postorder-traversal/
+
+ Given the root of a binary tree, return the postorder traversal of its nodes' values.
 
  Example 1:
      Input: root = [1,null,2,3]
-     Output: [1,2,3]
+     Output: [3,2,1]
  Example 2:
      Input: root = []
      Output: []
@@ -16,13 +17,13 @@ package easy;
      Output: [1]
  Example 4:
      Input: root = [1,2]
-     Output: [1,2]
+     Output: [2,1]
  Example 5:
      Input: root = [1,null,2]
-     Output: [1,2]
+     Output: [2,1]
 
  Constraints:
-     The number of nodes in the tree is in the range [0, 100].
+     The number of the nodes in the tree is in the range [0, 100].
      -100 <= Node.val <= 100
 
  Follow up: Recursive solution is trivial, could you do it iteratively?
@@ -53,77 +54,56 @@ import static org.hamcrest.Matchers.equalTo;
  *     }
  * }
  */
-public class BinaryTreePreorderTraversal {
-    public List<Integer> preorderTraversal(TreeNode root) {
+
+public class BinaryTreePostorderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
         if (root == null)
             return Arrays.asList();
 
         List<Integer> result = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();
         while(root != null || !stack.isEmpty()) {
-            if (root != null)
-                result.add(root.val);
-            else {
-                root = stack.pop();
-                root = root.right;
-                continue;
-            }
-
             if (root.left != null) {
                 stack.push(root);
                 root = root.left;
-            } else if (root.right != null) {
-                root = root.right;
             } else {
-                root = null;
+                if (root.right != null) {
+                    root = root.right;
+                } else {
+                    result.add(root.val);
+                    root = stack.pop();
+                    result.add(root.val);
+                    root = root.right;
+                }
             }
         }
         return result;
     }
 
-    public List<Integer> preorderTraversalReculsive(TreeNode root) {
-        if (root == null)
-            return Arrays.asList();
-
-        List<Integer> result = new ArrayList<>();
-
-        reculsive(root, result);
-
-        return result;
-    }
-
-    private void reculsive(TreeNode root, List<Integer> result) {
-        if (root == null) return;
-
-        result.add(root.val);
-        reculsive(root.left, result);
-        reculsive(root.right, result);
-    }
-
     @Test
     public void test() {
-        BinaryTreePreorderTraversal test = new BinaryTreePreorderTraversal();
+        BinaryTreePostorderTraversal test = new BinaryTreePostorderTraversal();
 
         TreeNode root = new TreeNode();
 
         // [1,null,2,3]
         root = new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null));
-        assertThat(test.preorderTraversal(root), equalTo(Arrays.asList(1,2,3)));
+        assertThat(test.postorderTraversal(root), equalTo(Arrays.asList(3,2,1)));
 
         root = null;
-        assertThat(test.preorderTraversal(root), equalTo(Arrays.asList()));
+        assertThat(test.postorderTraversal(root), equalTo(Arrays.asList()));
 
         root = new TreeNode(1);
-        assertThat(test.preorderTraversal(root), equalTo(Arrays.asList(1)));
+        assertThat(test.postorderTraversal(root), equalTo(Arrays.asList(1)));
 
         root = new TreeNode(1, new TreeNode(2), null);
-        assertThat(test.preorderTraversal(root), equalTo(Arrays.asList(1, 2)));
+        assertThat(test.postorderTraversal(root), equalTo(Arrays.asList(2,1)));
 
         root = new TreeNode(1, null, new TreeNode(2));
-        assertThat(test.preorderTraversal(root), equalTo(Arrays.asList(1, 2)));
+        assertThat(test.postorderTraversal(root), equalTo(Arrays.asList(2, 1)));
 
         // [1,4,3,2]
         root = new TreeNode(1, new TreeNode(4, new TreeNode(2), null), new TreeNode(3));
-        assertThat(test.preorderTraversal(root), equalTo(Arrays.asList(1,4,2,3)));
+        assertThat(test.postorderTraversal(root), equalTo(Arrays.asList(2,4,3,1)));
     }
 }
