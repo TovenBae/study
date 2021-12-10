6077fe08f492ade92ebce2e04930b09446205d50
TovenBae <furtur3@gmail.com>
2021-12-10 13:38:42 +0900

    add leetcode

--- LeetCode/src/main/java/easy/E709_ToLowerCase.java
+++ LeetCode/src/main/java/easy/E709_ToLowerCase.java
@@ -35,18 +35,36 @@ public class E709_ToLowerCase {
         return rtn;
     }
 
+    public String nuree(String s) {
+//        Scanner Scanner = new Scanner(System.in);                    //문자열 입력 받아서
+        String input_str = s;                        //입력 받은 문자열을 변수에 할당해주고
+        char[] char_arr = input_str.toCharArray();                    //문자열을 Char형태의 배열로 변환
+        String result = "";
+        for(int i=0; i<char_arr.length; ++i){
+            if(65<=char_arr[i] && char_arr[i]<=90){                    //문자열의 아스키코드의 값이 65~90의 알파벳 대문자라면
+                char_arr[i]=(char)(char_arr[i]+32);                    //32를 더해 소문자로 변환
+            }
+            result = result+Character.toString(char_arr[i]);    //결과를 담아준다
+        }
+        System.out.println(result);
+        return result;
+    }
+
     @Test
     public void testToLowerCase() {
         String s = "Hello";
         String output = "hello";
         assertEquals(output, toLowerCase(s));
+        assertEquals(output, nuree(s));
 
         s = "here";
         output = "here";
         assertEquals(output, toLowerCase(s));
+        assertEquals(output, nuree(s));
 
         s = "LOVELY";
         output = "lovely";
         assertEquals(output, toLowerCase(s));
+        assertEquals(output, nuree(s));
     }
 }
