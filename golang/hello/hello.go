2c7b3f4decbcaf314efbde1acea6e89f88aa9198
TovenBae <furtur3@gmail.com>
2024-02-23 18:08:03 +0900

    edu, goLang

--- golang/hello/hello.go
+++ golang/hello/hello.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"rsc.io/quote"
+) // 외부 모듈 호출추가
 
 func main() {
-  fmt.Println("Hello, World!");
+	fmt.Println("Hello, World!")
+	fmt.Println(quote.Go())
 }
-
